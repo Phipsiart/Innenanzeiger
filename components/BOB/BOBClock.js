5269f361c230725eb6e8e5f1c8d55bea63ccc3de
Phipsiart <98510944+Phipsiart@users.noreply.github.com>
2024-03-27 22:12:22 +0100

    make code readable (split the code to seperate files)

---
+++ components/BOB/BOBClock.js
@@ -0,0 +1,23 @@
+import { useState, useEffect } from "react";
+export default function BOBClock(){
+    const [time, setTime] = useState("");
+    const [blink, setBlink] = useState(false);  
+useEffect(() => {
+    const interval = setInterval(() => {
+      const now = new Date();
+      const hours = now.getHours().toString().padStart(2, "0");
+      const minutes = now.getMinutes().toString().padStart(2, "0");
+      const separator = blink ? ":" : " ";
+      setTime(`${hours}${separator}${minutes}`);
+      setBlink(!blink);
+    }, 1000);
+    return () => clearInterval(interval);
+  }, [blink]);
+  return(
+    <>
+          <span className="fixed end-3  text-[2rem]  text-white">
+        {time}
+      </span>
+    </>
+  )
+}
---
+++ components/BOB/BOBLines.js
@@ -0,0 +1,63 @@
+export default function BOBLines(){
+    return(
+        <>
+        <div className="fixed left-[13.9rem]">
+<svg width={84} height={750}>
+  <line x1={40} y1={62} x2={40} y2={80} stroke="#2c4958" strokeWidth={3} />
+  <rect width={20} height={20} x={30} y={79} fill="#2c4958" />
+  <line x1={40} y1={77} x2={40} y2={204} stroke="#2c4958" strokeWidth={3} />
+  <rect
+    width={20}
+    height={20}
+    x={30}
+    y={204}
+    stroke="#2c4958"
+    strokewidth={3}
+    fill="white"
+  />
+  <line x1={40} y1={224} x2={40} y2={290} stroke="#2c4958" strokeWidth={3} />
+  <rect
+    width={20}
+    height={20}
+    x={30}
+    y={289}
+    stroke="#2c4958"
+    strokewidth={3}
+    fill="white"
+  />
+  <line x1={40} y1={310} x2={40} y2={380} stroke="#2c4958" strokeWidth={3} />
+  <rect
+    width={20}
+    height={20}
+    x={30}
+    y={360}
+    stroke="#2c4958"
+    strokewidth={3}
+    fill="white"
+  />
+  <line x1={40} y1={380} x2={40} y2={465} stroke="#2c4958" strokeWidth={3} />
+  <rect
+    width={20}
+    height={20}
+    x={30}
+    y={438}
+    stroke="#2c4958"
+    strokewidth={3}
+    fill="white"
+  />
+  <line x1={40} y1={464} x2={40} y2={515} stroke="#2c4958" strokeWidth={3} />
+  <rect
+    width={20}
+    height={20}
+    x={30}
+    y={514}
+    stroke="#2c4958"
+    strokewidth={3}
+    fill="white"
+  />
+</svg>
+
+</div>
+</>
+    )
+}
