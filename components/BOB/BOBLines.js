8de9792a1c17542fe1c6835f3c33af4e98692f7d
Phipsiart <98510944+Phipsiart@users.noreply.github.com>
2024-03-28 00:08:34 +0100

    to many fixes to describe

--- components/BOB/BOBLines.js
+++ components/BOB/BOBLines.js
@@ -1,63 +1,52 @@
-export default function BOBLines(){
-    return(
-        <>
-        <div className="fixed left-[13.9rem]">
-<svg width={84} height={750}>
-  <line x1={40} y1={62} x2={40} y2={80} stroke="#2c4958" strokeWidth={3} />
-  <rect width={20} height={20} x={30} y={79} fill="#2c4958" />
-  <line x1={40} y1={77} x2={40} y2={204} stroke="#2c4958" strokeWidth={3} />
-  <rect
-    width={20}
-    height={20}
-    x={30}
-    y={204}
-    stroke="#2c4958"
-    strokewidth={3}
-    fill="white"
-  />
-  <line x1={40} y1={224} x2={40} y2={290} stroke="#2c4958" strokeWidth={3} />
-  <rect
-    width={20}
-    height={20}
-    x={30}
-    y={289}
-    stroke="#2c4958"
-    strokewidth={3}
-    fill="white"
-  />
-  <line x1={40} y1={310} x2={40} y2={380} stroke="#2c4958" strokeWidth={3} />
-  <rect
-    width={20}
-    height={20}
-    x={30}
-    y={360}
-    stroke="#2c4958"
-    strokewidth={3}
-    fill="white"
-  />
-  <line x1={40} y1={380} x2={40} y2={465} stroke="#2c4958" strokeWidth={3} />
-  <rect
-    width={20}
-    height={20}
-    x={30}
-    y={438}
-    stroke="#2c4958"
-    strokewidth={3}
-    fill="white"
-  />
-  <line x1={40} y1={464} x2={40} y2={515} stroke="#2c4958" strokeWidth={3} />
-  <rect
-    width={20}
-    height={20}
-    x={30}
-    y={514}
-    stroke="#2c4958"
-    strokewidth={3}
-    fill="white"
-  />
-</svg>
+import React from "react";
+export default function BOBLines({numLines}){
+  // If numLines is -1, return an empty component
+  if(numLines === -1) {
+    return null;
+}
 
-</div>
+// Define the maximum number of lines
+const maxLines = 5;
+
+// If numLines is greater than maxLines, set it to maxLines
+if(numLines > maxLines) {
+    numLines = maxLines;
+}
+
+// Create an array of lines and rectangles
+let linesAndRects = [];
+for(let i = 1; i <= numLines; i++) {
+    linesAndRects.push(
+      <React.Fragment key={i}>
+      <line x1={40} y1={67 + i*80} x2={40} y2={130 + i*80} stroke="#2c4958" strokeWidth={3} />
+      <rect width={20} height={20} x={30} y={127 + i*80} stroke="#2c4958" strokewidth="3" fill="white" />
+  </React.Fragment>
+    );
+}
+const OnlyOneStop = (
+  <>
+        <line x1={40} y1={3} x2={40} y2={80} stroke="#2c4958" strokeWidth={3} />
+  <rect width={20} height={20} x={30} y={79} fill="#2c4958" />
 </>
-    )
+)
+const OneStopLeft =(
+
+  <>
+        <line x1={40} y1={80} x2={40} y2={147} stroke="#2c4958" strokeWidth={3} />
+  <rect width={20} height={20} x={30} y={79} fill="#2c4958" />
+</>
+
+);
+
+return(
+    <>
+    <div className="fixed left-[13.9rem]">
+        <svg width={84} height={750}>
+        {numLines === 0 ? OnlyOneStop : null}
+        {numLines >= 1 ? OneStopLeft : null}
+            {numLines !== 0 && numLines !== -1 ? linesAndRects : null}
+        </svg>
+    </div>
+    </>
+)
 }
--- pages/departures.js
+++ pages/departures.js
@@ -18,71 +18,17 @@ export default function Home({ data }) {
   const Destination = data.Destination;
   const nextStopStatus = data.nextStopStatus;
   const stopovers = data.stopovers;
-  const closestStop = stopovers[data.closestStopIndex];
+  const closestStop = data.closestStopIndex !== -1 ? stopovers[data.closestStopIndex] : stopovers[stopovers.length - 1];
   var nextStops = stopovers.slice(data.closestStopIndex + 1, data.closestStopIndex + 6) || "Bad Leberkas";
+  console.log(nextStops.length)
   var closestStopName = "Bad Leberkas"
   if (closestStop && closestStop && typeof closestStop.name === 'string') {
     closestStopName = closestStop.name;
   }
-  
-  if (data.closestStopIndex === -1) {
-    const lastStop = stopovers[stopovers.length - 1]; // Get the last stop
-    return (
-      <>
-      <div
-      className="fixed top-0 h-12 z-[9999] bg-[#004e94] w-full"
-    >
-      <BOBClock />
-      <span className="fixed top-0 start-3 text-white text-[2rem]">{Line}</span>
-    </div>
-{/**Departures Board Start */}
-<div className="fixed left-[13.9rem]">
-<svg width={84} height={750}>
-  <line x1={40} y1={2} x2={40} y2={80} stroke="#2c4958" strokeWidth={3} />
-  <rect width={20} height={20} x={30} y={79} fill="#2c4958" />
-</svg>
-</div>
-<div className="fixed top-12 bg-white w-full h-[12rem] z-50">
-<p className="text-[8rem] text-center text-[#2c4958] ">Endstation</p>  
-</div>
-<div className="bg-[#d3d3d3] mt-[15rem] h-[52.5rem]  text-[#2c4958]">
-<div className="flex ml-[5.1rem] ">
-<div className="block ml-18 mt-1">
-  <p className="text-[1.3rem] text-center ">Plan</p>
-  <p className="mt-1 text-[3.5rem] font-bold ">{
-  (lastStop && typeof lastStop.plannedArrival === 'string')
-  ? lastStop.plannedArrival.slice(11, 16)
-  : "06:99"
-}
-</p>
-</div>
+  const ByeByeMessage = (
+    <p className="text-center fixed text-[#2c4958] top-[30rem] text-4xl">Dieser Zug endet dort. Wir bedanken uns für Ihre Reise und hoffen sie bald wieder an Board begrüßen zu dürfen.</p>
 
-<div className="block ml-[5.2rem] mt-1">
-  <p className="text-[1.3rem] text-center ">Aktuell</p>
-  <p className="mt-1 text-[3.5rem] ml-[28] font-bold text-green-600">{
-  (lastStop && typeof lastStop.arrival === 'string')
-    ? lastStop.arrival.slice(11, 16)
-    : "06:99"
-}
-  </p>
-</div>
-<div className="block ml-20 mt-9">
-  <p className="text-[1.3rem] text-left ">{nextStopStatus[data.closestStopIndex]}</p>
-  <p className="mt-1 text-[3.5rem] font-bold ">{lastStop.name}</p>
-</div>
-<div className="block fixed right-5 mt-1">
-  <p className="text-[1.3rem] text-left ">Gleis</p>
-  <p className="mt-1 text-[3.5rem] font-bold ">{lastStop.arrivalPlatform}</p>
-</div>
-</div>
-<div className="flex justify-center mt-20 text-4xl">
-  <p className="ml-20 mr-20">Wir bedanken uns für Ihre Reise mit der bayerischen Regiobahn und hoffen Sie bald wieder an Bord begrüßen zu dürfen.</p>
-</div>
-
-</div>
-          </>
-    );
-  }
+  )
   return (
     <>
       <Head>
@@ -108,58 +54,63 @@ export default function Home({ data }) {
     <div className="ml-20 mt-1">
   <p className="text-[1.3rem] text-center ">Plan</p>
   <p className="mt-1 text-[3.5rem] font-bold ">    
-  {closestStop['plannedDeparture']}
+  {data.closestStopIndex === -1 ? data.stopovers[data.totalstops - 1].plannedArrival : closestStop.plannedDeparture}
 
 </p>
 </div>
 {/**Place SVG next stop overlay here */}
-<BOBLines />
+<BOBLines numLines={data.stopsleft} />
 <div className="ml-[5.2rem] mt-1">
   <p className="text-[1.3rem] text-center ">Aktuell</p>
   <p className={`text-${closestStop['arrivalDelay'] > 300 ? 'red-500' : 'green-600'} mt-1 text-[3.5rem] font-bold text-green-600`}>
-    {closestStop['departure']}
+  {data.closestStopIndex === -1 ? data.stopovers[data.totalstops - 1].arrival : closestStop.departure}
   </p>
 </div>
 <div className="ml-20 mt-1">
-  <p className="text-[1.3rem] text-left ">{nextStopStatus[data.closestStopIndex]}</p>
+  <p className="text-[1.3rem] text-left ">{data.closestStopIndex === -1 ? 'Endstation' : nextStopStatus[data.closestStopIndex]}</p>
   <p className="mt-1 text-[3.5rem] font-bold ">{closestStopName}</p>
 </div>
 <div className="fixed right-5 mt-1">
   <p className="text-[1.3rem] text-left ">Gleis</p>
-  <p className="mt-1 text-[3.5rem] font-bold ">{closestStop.departurePlatform}</p>
+  <p className="mt-1 text-[3.5rem] font-bold ">
+  {data.closestStopIndex === -1 ? data.stopovers[data.totalstops - 1].arrivalPlatform : closestStop.departurePlatform}
+</p>
 </div>
 
     </div>
     {/**More departure cards start */}
     <div className="flex mt-8 mb-4 text-[#2c4958]">
     </div>
-    {nextStops.map((stopover, index) => (
-          <div key={index} className="flex text-[#2c4958]">
-            <div className="ml-24 mt-2">
-              <p className="mt-1 text-[2.6rem]">{
-  (stopover && typeof stopover.plannedDeparture === 'string')
-    ? stopover.plannedDeparture
-    : stopover.plannedArrival
-}
-</p>
-            </div>
-            <div className="ml-[7.6rem] mt-3.5">
-            <p className={`text-${stopover.arrivalDelay > 300 ? 'red-500' : 'green-600'} text-[2.6rem]`}>
-  {stopover && typeof stopover === 'string'
-    ? stopover.departure
-    : stopover.arrival}
-</p>
-
-            </div>
-            <div className="ml-[6.2rem] mt-3.5">
-              <p className="text-[2.5rem] ">{stopover.name}</p>
-            </div>
-          </div>
-        ))}
-
-
+    {data.closestStopIndex !== -1 && nextStops.map((stopover, index) => (
+  <div key={index} className="flex text-[#2c4958]">
+    <div className="ml-24 mt-2">
+      <p className="mt-1 text-[2.6rem]">{
+        (index === 0)
+          ? stopover.plannedArrival
+          : (stopover && typeof stopover.plannedArrival === 'string')
+            ? stopover.plannedDeparture
+            : stopover.plannedArrival
+      }
+      </p>
+    </div>
+    <div className="ml-[7.6rem] mt-3.5">
+      <p className={`text-${stopover.arrivalDelay > 300 ? 'red-500' : 'green-600'} text-[2.6rem]`}>
+        {index === -1
+          ? stopover.arrival
+          : (stopover && typeof stopover === 'string')
+            ? stopover.departure
+            : stopover.arrival}
+      </p>
+    </div>
+    <div className="ml-[6.2rem] mt-3.5">
+      <p className="text-[2.5rem] ">{stopover.name}</p>
+    </div>
+  </div>
+))}
         {/**More departure cards End */}
   </div>
+
+  {data.closestStopIndex === -1 ? ByeByeMessage : null}
       </main>
     </>
   );
