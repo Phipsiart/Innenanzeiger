1af24e1a14c01793679ec5565bdae8074115e0b2
Phipsiart <98510944+Phipsiart@users.noreply.github.com>
2024-04-05 17:03:36 +0200

    fixed time entering behavoiur
    when entering the time, only the date string was transferred to the connection page, this behavior has been fixed.

--- components/core/SearchInput.js
+++ components/core/SearchInput.js
@@ -14,21 +14,29 @@ import {
   PopoverTrigger,
 } from "@/components/ui/popover"
 
- export  function SearchInput(){
-  const router = useRouter();
-  function Redirect(){
+function Redirect() {
+    const router = useRouter()
     const from = document.getElementById("from").value || "München Hbf"
     const to = document.getElementById("to").value || "Berlin Hbf"
     console.log(from + to + date)
     router.push(`/connections?from=${from}&to=${to}&departure=${date}`)
-  }
-  const [date, setDate] = React.useState(new Date().toISOString());
-  const [time, setTime] = React.useState(() => {
+}
+
+const [date, setDate] = React.useState(new Date().toISOString());
+
+React.useEffect(() => {
+    const interval = setInterval(() => {
+        setDate(new Date().toISOString());
+    }, 1000);
+    return () => clearInterval(interval);
+}, []);
+
+const [time, setTime] = React.useState(() => {
     const date = new Date();
     const hours = date.getHours().toString().padStart(2, '0');
     const minutes = date.getMinutes().toString().padStart(2, '0');
     return `${hours}:${minutes}`;
-  });
+});
     console.log(date)
   return (
     <div className="flex justify-center mt-12 flex-wrap ">
